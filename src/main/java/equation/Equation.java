b1cfe3b4769609380a83347c2a24ec160352aee0
polacla1 <claus.polanka@s-itsolutions.at>
2019-03-02 19:39:34 +0100

    Add tests for Equation object

--- src/main/java/equation/Equation.java
+++ src/main/java/equation/Equation.java
@@ -1,11 +1,23 @@
 package equation;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Equation {
 
     private String equation;
 
     public Equation(String equation) {
+        validateFormatOf(equation);
         this.equation = equation;
+    }
+
+    private void validateFormatOf(String equation) {
+        Pattern p = Pattern.compile("(\\d)=(\\d)");
+        Matcher m = p.matcher(equation);
+        if (!m.matches()) {
+            throw new EquationInvalidFormatException("Equation must be in format: NR=NR");
+        }
     }
 
     public String leftSide() {
@@ -19,5 +31,12 @@ public class Equation {
     @Override
     public String toString() {
         return equation;
+    }
+
+    public class EquationInvalidFormatException extends RuntimeException {
+
+        public EquationInvalidFormatException(String msg) {
+            super(msg);
+        }
     }
 }
