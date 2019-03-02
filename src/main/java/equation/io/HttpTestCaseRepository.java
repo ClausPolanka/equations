a9a85bdc4a09829a5e746c470677687fd1ca6e87
polacla1 <claus.polanka@s-itsolutions.at>
2019-03-02 21:20:21 +0100

    Extract some methods

--- src/main/java/equation/io/HttpTestCaseRepository.java
+++ src/main/java/equation/io/HttpTestCaseRepository.java
@@ -16,25 +16,23 @@ import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static java.lang.String.format;
+
 public class HttpTestCaseRepository implements TestCaseRepository {
+
+    private static final String PROTOCOL = "http";
+    private static final String HOST = "localhost";
+    private static final int PORT = 8080;
+    private static final Pattern PATTERN = Pattern.compile(".*(\\d)=(\\d).*");
 
     public Equation getEquationFor(int testCase) {
         try {
-            URL url = new URL("http", "localhost", 8080, "/assignment/stage/1/testcase/" + testCase);
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
-            con.setRequestMethod("GET");
-            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-            String inputLine;
-            StringBuffer content = new StringBuffer();
-            while ((inputLine = in.readLine()) != null) {
-                content.append(inputLine);
-            }
-            in.close();
+            HttpURLConnection con = buildGetRequestConFor(testCase);
+            String responseBody = readResponseBodyFrom(con);
             con.disconnect();
-            Pattern p = Pattern.compile(".*(\\d)=(\\d).*");
-            Matcher m = p.matcher(content);
+            Matcher m = PATTERN.matcher(responseBody);
             m.matches();
-            return new Equation(m.group(1) + "=" + m.group(2));
+            return new Equation(format("%s=%s", m.group(1), m.group(2)));
         } catch (MalformedURLException e) {
             throw new RuntimeException();
         } catch (ProtocolException e) {
@@ -44,20 +42,33 @@ public class HttpTestCaseRepository implements TestCaseRepository {
         }
     }
 
+    private HttpURLConnection buildGetRequestConFor(int testCase) throws IOException {
+        URL url = new URL(PROTOCOL, HOST, PORT, pathToTestCase() + testCase);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        return con;
+    }
+
+    private String readResponseBodyFrom(HttpURLConnection con) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuilder responseBody = new StringBuilder();
+        while ((inputLine = in.readLine()) != null) {
+            responseBody.append(inputLine);
+        }
+        in.close();
+        return responseBody.toString();
+    }
+
+    private String pathToTestCase() {
+        return "/assignment/stage/1/testcase/";
+    }
+
     public void submitCorrectedEquationsFor(int testCase, Set<Equation> correctedEquations) {
         try {
-            URL url = new URL("http", "localhost", 8080, "/assignment/stage/1/testcase/" + testCase);
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
-            con.setRequestMethod("POST");
-            con.setDoOutput(true);
-            StringBuilder requestBody = new StringBuilder();
-            requestBody.append("{");
-            requestBody.append("\"correctedEquations\":").append(correctedEquations);
-            requestBody.append("}");
-            int length = requestBody.length();
-
-            con.setFixedLengthStreamingMode(length);
-            con.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+            HttpURLConnection con = buildPostRequestConFor(testCase);
+            StringBuilder requestBody = buildRequestBodyFor(correctedEquations);
+            con.setFixedLengthStreamingMode(requestBody.length());
             con.connect();
             try (OutputStream os = con.getOutputStream()) {
                 os.write(requestBody.toString().getBytes(StandardCharsets.UTF_8));
@@ -65,5 +76,22 @@ public class HttpTestCaseRepository implements TestCaseRepository {
         } catch (IOException e) {
             throw new RuntimeException();
         }
+    }
+
+    private HttpURLConnection buildPostRequestConFor(int testCase) throws IOException {
+        URL url = new URL(PROTOCOL, HOST, PORT, pathToTestCase() + testCase);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("POST");
+        con.setDoOutput(true);
+        con.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+        return con;
+    }
+
+    private StringBuilder buildRequestBodyFor(Set<Equation> correctedEquations) {
+        StringBuilder requestBody = new StringBuilder();
+        requestBody.append("{");
+        requestBody.append("\"correctedEquations\":").append(correctedEquations);
+        requestBody.append("}");
+        return requestBody;
     }
 }
