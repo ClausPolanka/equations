f7843904a0539705e4767ee39b7d3d7c76621431
polacla1 <claus.polanka@s-itsolutions.at>
2019-03-02 18:54:23 +0100

    Introduce Equation object

--- src/main/java/equation/EquationSolver.java
+++ src/main/java/equation/EquationSolver.java
@@ -5,7 +5,9 @@ import java.util.Hashtable;
 import java.util.List;
 import java.util.Set;
 
+import static java.lang.String.format;
 import static java.util.Arrays.asList;
+import static java.util.Collections.singletonList;
 
 public class EquationSolver {
 
@@ -14,35 +16,27 @@ public class EquationSolver {
     private static Hashtable<String, List<String>> solutions() {
         Hashtable<String, List<String>> solutionSpace = new Hashtable<>();
         solutionSpace.put("0", asList("0", "6", "9"));
-        solutionSpace.put("1", asList("1"));
+        solutionSpace.put("1", singletonList("1"));
         solutionSpace.put("2", asList("2", "3"));
         solutionSpace.put("3", asList("2", "3", "5"));
-        solutionSpace.put("4", asList("4"));
+        solutionSpace.put("4", singletonList("4"));
         solutionSpace.put("5", asList("3", "5"));
         solutionSpace.put("6", asList("0", "6", "9"));
-        solutionSpace.put("7", asList("7"));
-        solutionSpace.put("8", asList("8"));
+        solutionSpace.put("7", singletonList("7"));
+        solutionSpace.put("8", singletonList("8"));
         solutionSpace.put("9", asList("0", "6", "9"));
         return solutionSpace;
     }
 
-    public static Set<String> solveEquation(String equation) {
+    public static Set<String> solveEquation(Equation e) {
         Set<String> correctedEquations = new HashSet<>();
 
-        if (solutionSpace.get(leftSideOf(equation)).contains(rightSideOf(equation)))
-            correctedEquations.add(String.format("\"%s=%s\"", rightSideOf(equation), rightSideOf(equation)));
+        if (solutionSpace.get(e.leftSide()).contains(e.rightSide()))
+            correctedEquations.add(format("\"%s=%s\"", e.rightSide(), e.rightSide()));
 
-        if (solutionSpace.get(rightSideOf(equation)).contains(leftSideOf(equation)))
-            correctedEquations.add(String.format("\"%s=%s\"", leftSideOf(equation), leftSideOf(equation)));
+        if (solutionSpace.get(e.rightSide()).contains(e.leftSide()))
+            correctedEquations.add(format("\"%s=%s\"", e.leftSide(), e.leftSide()));
 
         return correctedEquations;
-    }
-
-    public static String leftSideOf(String equation) {
-        return equation.split("=")[0];
-    }
-
-    public static String rightSideOf(String equation) {
-        return equation.split("=")[1];
     }
 }
