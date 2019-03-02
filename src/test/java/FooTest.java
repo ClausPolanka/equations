20a499fa2973d2b37ddf8a43fe4e96ee6e4b2ab8
polacla1 <claus.polanka@s-itsolutions.at>
2019-03-02 13:46:04 +0100

    Create project skeleton

---
+++ src/test/java/FooTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FooTest {
+
+    @Test
+    public void foo() {
+        assertEquals("foo", true, true);
+    }
+}
