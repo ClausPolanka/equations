6b47079d6521e677195de0e1ef0a4a9960e78c11
ClausPolanka <claus.polanka@chello.at>
2019-05-31 14:42:43 +0200

    Extend test cases for input equations with no solution (0-2)

--- src/test/java/EquationsTest.java
+++ src/test/java/EquationsTest.java
@@ -21,7 +21,9 @@ public class EquationsTest {
     }
 
     @TestWith({
-            "0=1"
+            "0=1", "0=2", "0=3", "0=4", "0=5", "0=7", "0=8",
+            "1=2", "1=3", "1=4", "1=5", "1=6", "1=7", "1=8", "1=9",
+            "2=4", "2=5", "2=6", "2=7", "2=8", "2=9",
     })
     public void findCorrectEquationsReturnsEmptyListWhenThereIsNoSolution(String inputEquation) {
         Set<String> result = new Equations().findCorrectEquations(inputEquation);
