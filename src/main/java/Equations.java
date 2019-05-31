0a546671b94ec64a3494991fd22cbdc783e2bb14
cmacher <chris.macher@gmail.com>
2019-05-31 15:51:24 +0200

    Extract solution space as a constant

--- src/main/java/Equations.java
+++ src/main/java/Equations.java
@@ -9,13 +9,9 @@ import static java.util.Collections.singletonList;
 
 public class Equations {
 
-    public Set<String> findCorrectEquations(String equation) {
-        final String[] digits = parse(equation);
-        return findCorrectEquations(digits[0], digits[1]);
+    private static final Map<String, List<String>> SOLUTIONS = createSolutionSpace();
 
-    }
-
-    private Set<String> findCorrectEquations(String digit1, String digit2) {
+    private static Map<String, List<String>> createSolutionSpace() {
         Map<String, List<String>> solutions = new HashMap<>();
         solutions.put("0", asList("0", "6", "9"));
         solutions.put("1", singletonList("1"));
@@ -27,9 +23,17 @@ public class Equations {
         solutions.put("7", singletonList("7"));
         solutions.put("8", singletonList("8"));
         solutions.put("9", asList("0", "6", "9"));
+        return solutions;
+    }
 
+    public Set<String> findCorrectEquations(String equation) {
+        final String[] digits = parse(equation);
+        return findCorrectEquations(digits[0], digits[1]);
+    }
+
+    private Set<String> findCorrectEquations(String digit1, String digit2) {
         Set<String> correctEquations = new HashSet<>();
-        if (solutions.get(digit1).contains(digit2)) {
+        if (SOLUTIONS.get(digit1).contains(digit2)) {
             correctEquations.add(digit1 + "=" + digit1);
             correctEquations.add(digit2 + "=" + digit2);
         }
