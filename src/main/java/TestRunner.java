257f0a70f3ee77b35289b5ef16ac534d3d974327
Shubhangi Joshi <shubhangi.joshi@erstegroup.com>
2019-06-14 14:38:16 +0200

    Extract http con and request execution into separate methods

--- src/main/java/TestRunner.java
+++ src/main/java/TestRunner.java
@@ -17,11 +17,20 @@ public class TestRunner {
     }
 
     private static void runTestCase(int testcase) throws IOException {
+        HttpURLConnection con = createHttpGetConnection(testcase);
+
+        String jsonResponse = executeRequest(con);
+        System.out.println(jsonResponse);
+    }
+
+    private static HttpURLConnection createHttpGetConnection(int testcase) throws IOException {
         URL url = new URL("http://localhost:8080/assignment/stage/1/testcase/" + testcase);
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setRequestMethod("GET");
-        int status = con.getResponseCode();
-        System.out.println(status);
+        return con;
+    }
+
+    private static String executeRequest(HttpURLConnection con) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
         String inputLine;
         StringBuffer content = new StringBuffer();
@@ -31,6 +40,7 @@ public class TestRunner {
         System.out.println(content);
         in.close();
         con.disconnect();
+        return content.toString();
     }
 
 }
