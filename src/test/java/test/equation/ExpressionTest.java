376d2a5e17453a560b62f75294c43d7cd833bf67
Shubhangi Joshi <shubhangi.joshi@erstegroup.com>
2019-07-19 11:38:25 +0200

    Remove unnecessary test

--- src/test/java/test/equation/ExpressionTest.java
+++ src/test/java/test/equation/ExpressionTest.java
@@ -1,26 +1,13 @@
 package test.equation;
 
 import equation.Expression;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class ExpressionTest {
-
-    @Rule
-    public ExpectedException exception = ExpectedException.none();
-
-    @Test
-    public void createNewExpressionReturnsExpressionForAGivenValidExpressionString() {
-        Expression e = new Expression("0+1");
-        assertThat(e.operand1, is(equalTo("0")));
-        assertThat(e.operand2, is(equalTo("1")));
-        assertThat(e.operator, is(equalTo("+")));
-    }
 
     @Test
     public void evaluateEvaluatesSingleDigitExpression() {
